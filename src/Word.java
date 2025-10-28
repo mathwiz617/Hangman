79c0e34b3e67a683eb4113494783d5c8a2a7c068
mathwiz617 <mathwiz@me.com>
2025-10-28 13:33:22 -0400

    First part of code written

---
+++ src/Word.java
@@ -0,0 +1,69 @@
+import java.util.ArrayList;
+import java.util.Scanner;
+import java.util.Random;
+import java.io.*;
+
+public class Word{
+	private	ArrayList<String> wordList; 
+	private String word;
+	private	char[] goal;
+	
+	public Word(){
+		wordList = new ArrayList<>();
+		word = "";
+	}
+	
+	public char[] getGoal(){
+		return goal;
+	}
+	
+	public void setGoal(char[] letters){
+		goal = letters;
+		return;
+	}
+	
+	public String getWord(){
+		return word;
+	}
+	
+	public void setWord(String string){
+		word = string;
+		return;
+	}
+	
+	public ArrayList<String> buildList(){
+		return wordList;
+	}
+	
+	public void setGoal(ArrayList<String> list){
+		wordList = list;
+		return;
+	}
+	
+	public void load(String file) throws FileNotFoundException{
+		String fileName = file;
+		File loadFrom = new File(fileName);
+		Scanner inputFile = new Scanner(loadFrom);
+		
+		while(inputFile.hasNext()){
+			String nextWord = inputFile.nextLine();
+			wordList.add(nextWord.trim());
+		}
+		inputFile.close();
+		return;
+	}
+	
+	public void chooseWord(){
+		int size = wordList.size();
+		int index;
+		Random rand = new Random();
+		index = rand.nextInt(size);
+		word = wordList.get(index);
+		return;
+	}
+	
+	public char[] generateGoal(){
+		goal = word.toCharArray();
+		return goal;
+	}
+}
