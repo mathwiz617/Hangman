e57499c440ae9d1d63d6aeff5012a1c74973edcd
mathwiz617 <mathwiz@me.com>
2025-10-30 13:58:12 -0400

    Starting code from pseudocode

--- src/Guess.java
+++ src/Guess.java
@@ -1,4 +1,49 @@
 
 public class Guess {
 
+	private char[] goal;
+	private char letterGuessed = ' ';
+	private char[] lettersGuessedOld;
+	private char[] lettersGuessedNew;
+
+	
+	public Guess(char[] word) {
+		goal = word;
+	}
+	
+	public char getLetterGuessed() {
+		return letterGuessed;
+	}
+
+	public void setLetterGuessed(char letterGuessed) {
+		this.letterGuessed = letterGuessed;
+	}
+
+	public char[] getLettersGuessedOld() {
+		return lettersGuessedOld;
+	}
+
+	public void setLettersGuessedOld(char[] lettersGuessedOld) {
+		this.lettersGuessedOld = lettersGuessedOld;
+	}
+
+	public char[] getLettersGuessedNew() {
+		return lettersGuessedNew;
+	}
+
+	public void setLettersGuessedNew(char[] lettersGuessedNew) {
+		this.lettersGuessedNew = lettersGuessedNew;
+	}
+	
+	public void start() {
+		lettersGuessedOld = new char[goal.length];
+		lettersGuessedNew = new char[goal.length];
+		
+		for (int i = 0; i < goal.length; i++) {
+			lettersGuessedOld[i] = '_';
+			lettersGuessedNew[i] = '_';
+		}
+	}
+	
+	
 }
