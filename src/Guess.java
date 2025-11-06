1d3fb94ce6dcc4e87096c1ad12f6c35ab5a642d9
singerrachel77 <singerrachel77@gmail.com>
2025-11-06 13:08:24 -0500

    w

--- src/Guess.java
+++ src/Guess.java
@@ -1,7 +1,16 @@
 import java.util.Arrays;
 
 public class Guess {
+	//initialize variables used in guessing process
+	private char[] goal; //word to guess
+	private char letterGuessed = ' '; //letter guessed by user
+	private String wordGuessedString = ""; //word guessed by user
+	private char[] wordGuessed; //word guessed as an array
+	private char[] lettersGuessedOld; // keep track of accurate guesses
+	private char[] lettersGuessedNew; // array to compare 
+	int wrong = 0; //how many wrong guesses from user
 
+<<<<<<< Updated upstream
 	private char[] goal;
 	private char letterGuessed = ' ';
 	private String wordGuessedString = "";
@@ -12,49 +21,59 @@ public class Guess {
 	private boolean loss = false;
 
 	
+=======
+	//constructor to for hangman word
+>>>>>>> Stashed changes
 	public Guess(char[] word) {
 		goal = word;
 	}
-	
+	//constructor for letter guessed
 	public Guess(char letter) {
 		letterGuessed = letter;
 	}
 	
+	//letter guessed getter
 	public char getLetterGuessed() {
 		return letterGuessed;
 	}
-
+	
+	//letter guessed setter
 	public void setLetterGuessed(char letterGuessed) {
 		this.letterGuessed = letterGuessed;
 	}
+	
+	//construuctor for guessing word
 	public Guess(String word) {
 		wordGuessedString = word;
 	}
 	
+	//get word guessed
 	public String getWordGuessed() {
 		return wordGuessedString;
 	}
-
+	//set word guessed
 	public void setWordGuessed(String wordGuessedString) {
 		this.wordGuessedString = wordGuessedString;
 	}
-
+	
+	//get array for the old progress of word
 	public char[] getLettersGuessedOld() {
 		return lettersGuessedOld;
 	}
-
+	//set
 	public void setLettersGuessedOld(char[] lettersGuessedOld) {
 		this.lettersGuessedOld = lettersGuessedOld;
 	}
-
+	//get new progress for letters guessed
 	public char[] getLettersGuessedNew() {
 		return lettersGuessedNew;
 	}
-
+	//set
 	public void setLettersGuessedNew(char[] lettersGuessedNew) {
 		this.lettersGuessedNew = lettersGuessedNew;
 	}
 	
+<<<<<<< Updated upstream
 	public int getWrong() {
 		return wrong;
 	}
@@ -75,16 +94,22 @@ public class Guess {
 		this.loss = loss;
 	}
 
+=======
+	//method after starting game
+>>>>>>> Stashed changes
 	public void start() {
+		//create the letters guessed displays the size of goal word
 		lettersGuessedOld = new char[goal.length];
 		lettersGuessedNew = new char[goal.length];
 		
+		//make the arrays start as blank
 		for (int i = 0; i < goal.length; i++) {
 			lettersGuessedOld[i] = '_';
 			lettersGuessedNew[i] = '_';
 		}
 	}
 			
+<<<<<<< Updated upstream
 	public void letterGuess(char a) {
 		for (int i = 0; i < goal.length; i++) {
 			if (a == goal[i]) {
@@ -94,11 +119,24 @@ public class Guess {
 		if (Arrays.equals(lettersGuessedOld, lettersGuessedNew)) {
 			wrong++;
 			System.out.println("That letter is not in this word!");
+=======
+		//check each index if the letter guessed is in the goal word and replace blank
+		for (int i = 0; i < goal.length; i++) {
+			if (letterGuessed == goal[i]) {
+				lettersGuessedNew[i] = letterGuessed; //array track accurate guesses
+			}
+		}
+		
+		//compare arrays to see if they got a guess wrong
+		if (lettersGuessedOld.equals(lettersGuessedNew)) {
+			wrong++; //track wrong
+>>>>>>> Stashed changes
 			System.out.println(lettersGuessedOld);
 			wrong();
 			}
-		else {
+		else { //got it right
 			System.out.println(lettersGuessedNew);
+			//reset arrays for future guesses
 			for (int i = 0; i < goal.length; i++) {
 				lettersGuessedOld[i] = lettersGuessedNew[i];
 			}
